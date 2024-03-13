b7bcfeb835479ce83c82873a77753816cc932f51
Mykyta Barabanov <deaneygaming@gmail.com>
2024-03-13 13:37:30 +0200

    Finished CoinInfoModal

--- src/components/CoinInfoModal.jsx
+++ src/components/CoinInfoModal.jsx
@@ -1,3 +1,58 @@
+import { Divider, Flex, Tag, Typography } from 'antd'
+import { formatNumber } from '../utils'
+
 export default function CoinInfoModal({ coin }) {
-	return <h2>{coin.name}</h2>
+	return (
+		<>
+			<Flex align='center'>
+				<img
+					src={coin.icon}
+					alt={coin.name}
+					style={{ width: 40, marginRight: 10 }}
+				/>
+				<Typography.Title level={2} style={{ margin: 0 }}>
+					({coin.symbol}) {coin.name}
+				</Typography.Title>
+			</Flex>
+			<Divider />
+			<Flex>
+				<Typography.Paragraph>
+					<Typography.Text strong>1 Hour:</Typography.Text>
+					<Tag color={coin.priceChange1h > 0 ? 'green' : 'red'}>
+						{coin.priceChange1h}%
+					</Tag>
+				</Typography.Paragraph>
+				<Typography.Paragraph>
+					<Typography.Text strong>1 Day:</Typography.Text>
+					<Tag color={coin.priceChange1d > 0 ? 'green' : 'red'}>
+						{coin.priceChange1d}%
+					</Tag>
+				</Typography.Paragraph>
+				<Typography.Paragraph>
+					<Typography.Text strong>1 Weak:</Typography.Text>
+					<Tag color={coin.priceChange1w > 0 ? 'green' : 'red'}>
+						{coin.priceChange1w}%
+					</Tag>
+				</Typography.Paragraph>
+			</Flex>
+			<Typography.Paragraph>
+				<Typography.Text strong>Price: </Typography.Text>
+				{coin.price.toFixed(2)}$
+			</Typography.Paragraph>
+			<Typography.Paragraph>
+				<Typography.Text strong>Price BTC: </Typography.Text>
+				{coin.priceBtc.toFixed(10)}
+			</Typography.Paragraph>
+			<Typography.Paragraph>
+				<Typography.Text strong>Market Cap: </Typography.Text>
+				{formatNumber(coin.marketCap)}$
+			</Typography.Paragraph>
+			{coin.contractAddress && (
+				<Typography.Paragraph>
+					<Typography.Text strong>Contract Address:</Typography.Text>
+					{coin.contractAddress}
+				</Typography.Paragraph>
+			)}
+		</>
+	)
 }
--- src/utils.js
+++ src/utils.js
@@ -5,3 +5,16 @@ export function percentDifference(a, b) {
 export function capitalize(str) {
 	return str.charAt(0).toUpperCase() + str.substr(1)
 }
+
+export function formatNumber(num) {
+	if (Math.abs(num) < 999999) return num
+	const suffixes = ['', 'K', 'M', 'B', 'T']
+	const sign = Math.sign(num)
+	const absNum = Math.abs(num)
+
+	const suffixIndex = Math.floor(Math.log10(absNum) / 3)
+
+	const scaledNum = absNum / Math.pow(10, suffixIndex * 3)
+
+	return (sign * scaledNum).toFixed(1) + suffixes[suffixIndex]
+}
